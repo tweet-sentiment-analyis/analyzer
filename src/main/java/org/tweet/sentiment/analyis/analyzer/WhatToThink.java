0abc8fa5282f1334a231430b2ded300fb54c4229
Raphael <raweibel@student.ethz.ch>
2017-03-21 20:39:18 +0100

    Added a json reader for testing

--- src/main/java/org/tweet/sentiment/analyis/analyzer/WhatToThink.java
+++ src/main/java/org/tweet/sentiment/analyis/analyzer/WhatToThink.java
@@ -1,23 +1,39 @@
 package org.tweet.sentiment.analyis.analyzer;
 
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class WhatToThink {
 
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		NLP.init();
 		long afterInit = System.currentTimeMillis();
-		String tweet = "Finally, an explanation for daylight savings that makes sense";
-		System.out.println(tweet + " : " + NLP.findSentiment(tweet));
-		long b = System.currentTimeMillis();
-		String te = "RT @NRDC: These climate programs would be axed under Trump's proposed budget. https:t.coijRd56F7uI via @insideclimate";
-		System.out.println(te + " : " + NLP.findSentiment(te));
-		
-		long c = System.currentTimeMillis();
-		te = "The news media spent SIX HUNDRED CONSECUTIVE DAYS on Hillary's emails. No Trump story, however egregious, got e\u2026";
-		System.out.println(te + " : " + NLP.findSentiment(te));
-		System.out.println(System.currentTimeMillis()-start);
-		System.out.println(System.currentTimeMillis()-afterInit);
-		System.out.println(System.currentTimeMillis()-b);
-		System.out.println(System.currentTimeMillis()-c);
+
+		JSONParser parser = new JSONParser();
+
+		try {
+
+			Object obj = parser.parse(new FileReader(
+					"/Users/Raphael/Documents/UZH/2_Semester/testMessage.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			String text = (String) jsonObject.get("text");
+            long a = System.currentTimeMillis();
+			System.out.println(text + " : " + + NLP.findSentiment(text));
+            long c = System.currentTimeMillis();
+            System.out.println(c-a);
+            System.out.println(afterInit-start);
+
+            String msg = jsonObject.toJSONString();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
